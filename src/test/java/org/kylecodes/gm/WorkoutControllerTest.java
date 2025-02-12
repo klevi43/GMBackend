e5ae27c76e9899a798959b3b11e62b0c35bb32cd
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-02-12 15:57:03 +0900

    Added MediaType to specify JSON WorkoutControllerTest

--- src/test/java/org/kylecodes/gm/WorkoutControllerTest.java
+++ src/test/java/org/kylecodes/gm/WorkoutControllerTest.java
@@ -17,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -44,6 +45,9 @@ public class WorkoutControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+
+    public static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON;
+
 
 
     private Workout test1 = new Workout( "test1", new Date());
