2a248987529215ca7f5814098a1df866e6ae1ff5
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-02-14 14:44:47 +0900

    Tested Workout repository save all

--- src/test/java/org/kylecodes/gm/repositories/WorkoutRepositoryTest.java
+++ src/test/java/org/kylecodes/gm/repositories/WorkoutRepositoryTest.java
@@ -7,10 +7,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
+@AutoConfigureTestDatabase(replace =AutoConfigureTestDatabase.Replace.NONE )
 @TestPropertySource("classpath:application-test.properties")
 public class WorkoutRepositoryTest {
     @Autowired
@@ -19,12 +21,20 @@ public class WorkoutRepositoryTest {
     @Test
     public void WorkoutRepository_SaveAll_ReturnSavedWorkouts() {
         // Arrange
-        Workout workout = new Workout();
-        workout.setName("Chest Day");
-        workout.setDate(new Date());
+        Workout workout1 = new Workout();
+        workout1.setName("Chest Day");
+        workout1.setDate(LocalDateTime.now());
+
+        Workout workout2 = new Workout();
+        workout2.setName("Chest Day");
+        workout2.setDate(LocalDateTime.now());
+
         //Act
+        Workout savedWorkout1 = workoutRepository.save(workout1);
+        Workout savedWorkout2 = workoutRepository.save(workout2);
 
         // Assert
-
+        assertThat(savedWorkout1).isNotNull();
+        assertThat(savedWorkout2).isNotNull();
     }
 }
