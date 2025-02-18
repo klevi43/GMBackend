adc070de75c72caa9197d478df862581fc88f605
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-02-18 20:19:40 +0900

    Added first test for create workout

--- src/test/java/org/kylecodes/gm/controllers/WorkoutControllerTest.java
+++ src/test/java/org/kylecodes/gm/controllers/WorkoutControllerTest.java
@@ -1,51 +1,69 @@
 package org.kylecodes.gm.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.kylecodes.gm.dtos.WorkoutDto;
 import org.kylecodes.gm.entities.Workout;
 import org.kylecodes.gm.services.WorkoutService;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 
 @TestPropertySource("classpath:application-test.properties")
+@SpringBootTest
 @AutoConfigureMockMvc
-@SpringBootTest // loads entire app context
-@Transactional
 @ExtendWith(MockitoExtension.class)
 public class WorkoutControllerTest {
-    private static MockHttpServletRequest req;
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Mock
-    private WorkoutService workoutServiceMock;
-
-    @InjectMocks
-    private Workout workout;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Mock
+    private WorkoutService workoutServiceMock;
+
     @Autowired
     ObjectMapper objectMapper;
 
-    public static final MediaType APPLICATION_JSON_UTF8 = APPLICATION_JSON;
 
+    private Workout workout;
+    private WorkoutDto workoutDto;
 
+    @BeforeEach
+    public void init() {
+        workout = new Workout();
+        workout.setName("Chest Day");
+        workout.setDate(LocalDate.now());
+
+        workoutDto = new WorkoutDto();
+        workoutDto.setName("Chest Day");
+        workoutDto.setDate(LocalDate.now());
+    }
+
+    @Test
+    public void WorkoutController_CreateWorkout_ReturnCreated() throws Exception {
+        //given(workoutServiceMock.createWorkout(ArgumentMatchers.any())).willAnswer(invocationOnMock -> invocationOnMock.getArgument());
+
+        ResultActions response = mockMvc.perform(post("/workouts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(workoutDto)));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated());
+    }
 
 
 }
