f3c63b169bee837299cc3c92c7cea159df3ad322
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-03-03 11:10:49 +0900

    Defined EntityToDtoMapper for Set/SetDto

---
+++ src/main/java/org/kylecodes/gm/services/mappers/SetToSetDtoMapper.java
@@ -0,0 +1,15 @@
+package org.kylecodes.gm.services.mappers;
+
+import org.kylecodes.gm.dtos.SetDto;
+import org.kylecodes.gm.entities.Set;
+
+public class SetToSetDtoMapper implements EntityToDtoMapper<Set, SetDto> {
+    @Override
+    public SetDto mapToDto(Set set) {
+        SetDto setDto = new SetDto();
+        setDto.setId(set.getId());
+        setDto.setWeight(set.getWeight());
+        setDto.setReps(set.getReps());
+        return setDto;
+    }
+}
