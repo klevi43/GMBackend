4ac19e3869c5791307a593e3b26bcded55b5ce00
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-03-29 15:50:28 +0900

    Created UserToRegisterDtoMapper

---
+++ src/main/java/org/kylecodes/gm/mappers/UserToRegisterDtoMapper.java
@@ -0,0 +1,16 @@
+package org.kylecodes.gm.mappers;
+
+import org.kylecodes.gm.dtos.RegisterDto;
+import org.kylecodes.gm.entities.User;
+
+public class UserToRegisterDtoMapper implements EntityToDtoMapper<User, RegisterDto> {
+    @Override
+    public RegisterDto mapToDto(User user) {
+        RegisterDto userDto = new RegisterDto();
+        userDto.setId(user.getId());
+        userDto.setEmail(user.getUsername());
+        userDto.setRole(user.getRole());
+
+        return userDto;
+    }
+}
