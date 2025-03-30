df5e9ea01873852182974e2402b103e59ffabdce
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-03-30 13:00:38 +0900

    Moved Login to AuthController

--- src/main/java/org/kylecodes/gm/controllers/UserController.java
+++ src/main/java/org/kylecodes/gm/controllers/UserController.java
@@ -1,8 +1,6 @@
 package org.kylecodes.gm.controllers;
 
 import org.kylecodes.gm.dtos.RegisterDto;
-import org.kylecodes.gm.dtos.UserDto;
-import org.kylecodes.gm.services.AuthServiceImpl;
 import org.kylecodes.gm.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,17 +12,11 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @Autowired
-    AuthServiceImpl authService;
     @PostMapping("/sign-up")
     public RegisterDto register(@RequestBody RegisterDto registerDto) {
         System.out.println(registerDto.getEmail());
         return userService.registerNewUser(registerDto);
     }
 
-    @PostMapping("/login")
-    public String login(@RequestBody UserDto user) {
 
-        return authService.verify(user);
-    }
 }
