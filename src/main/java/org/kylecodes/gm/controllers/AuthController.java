936bd63b3a714dc3afa56a7a49ab09938a1aa3c9
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-05-17 19:43:17 +0900

    implemented logout functionality

--- src/main/java/org/kylecodes/gm/controllers/AuthController.java
+++ src/main/java/org/kylecodes/gm/controllers/AuthController.java
@@ -1,7 +1,6 @@
 package org.kylecodes.gm.controllers;
 
 import jakarta.servlet.http.HttpServletResponse;
-import org.kylecodes.gm.constants.TokenDuration;
 import org.kylecodes.gm.dtos.AuthUserDto;
 import org.kylecodes.gm.dtos.JwtDto;
 import org.kylecodes.gm.responses.LoginResponse;
@@ -19,21 +18,33 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
-
-    @PostMapping(value = "/login")
+    @PostMapping("/auth/logout")
+    public void logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("jwtToken", "")
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+    }
+    @PostMapping(value = "/auth/login")
     public ResponseEntity<?> login(@RequestBody AuthUserDto authUserDto, HttpServletResponse response) {
 
         JwtDto jwtDto = authService.verify(authUserDto);
 
         ResponseCookie cookie = ResponseCookie.from("jwtToken", jwtDto.getPayload())
                 .httpOnly(true)
-                .secure(true)
+                .secure(false)
                 .path("/")
-                .maxAge(TokenDuration.THIRTY_MINS)
+                .maxAge(1800)
                 .sameSite("Strict")
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
         return ResponseEntity.ok(new LoginResponse("Success"));
     }
+
+
 }
