46a7c9fbd79dc331e304cab5759323d051481a42
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-18 22:29:36 +0900

    Added componet annotatino

--- src/main/java/org/kylecodes/gm/filters/CsrfLoggingFilter.java
+++ src/main/java/org/kylecodes/gm/filters/CsrfLoggingFilter.java
@@ -5,11 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
-
+@Component
 public class CsrfLoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
