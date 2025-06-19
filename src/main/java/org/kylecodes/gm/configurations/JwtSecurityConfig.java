9302c6af53a65f248435f0a8a02cdb541022b9d0
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-19 18:52:06 +0900

    Temporarily disabled csrf

--- src/main/java/org/kylecodes/gm/configurations/JwtSecurityConfig.java
+++ src/main/java/org/kylecodes/gm/configurations/JwtSecurityConfig.java
@@ -1,12 +1,12 @@
 package org.kylecodes.gm.configurations;
 
-import org.kylecodes.gm.filters.CsrfLoggingFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.kylecodes.gm.filters.JwtFilter;
 import org.kylecodes.gm.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,24 +17,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.*;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
-import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
-import org.springframework.util.StringUtils;
 import java.util.List;
+import java.util.function.Supplier;
 
 @Configuration
 @EnableWebSecurity
@@ -140,26 +132,7 @@ public class JwtSecurityConfig {
             }
         };
     }
-    /*
-    never store credentials in memory!!!
-    encoding vs hashing vs encryption
 
-    encoding: transform data from one form to another (usually more efficient) form. not used for securing data.
-    Typically used to compress or stream data. ex: base 64, WAV, Mp3
-
-    hashing: convert data into a hash
-        - one way process, not reversible
-        - use it to validate integrity of data
-    e.g. send request of data and hashed data. The recipient hashes the request's data, and them compares it
-    with the request's provided hashed data. if they are the same, then the data is valid and has not
-    been manipulated
-    ex: bcrypt, scrypt
-
-    encryption: encoding data using a key or password
-        - a key or password is necessary to decrypt
-        - used to safeguard data
-    ex: RSA (Rivest-Shamir-Adleman) public key cryptography system.
-    */
 
     @Bean
     public PasswordEncoder passwordEncoder() {
