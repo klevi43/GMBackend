6535c7ea0d5500d0d9a3037df4c1f3dda70a5740
Kyle Levisay <kyleandrewlevi43@outlook.com>
2025-06-21 19:32:22 +0900

    Extracted SpaCsrfTokenRequestHandler to its own separate class

--- src/main/java/org/kylecodes/gm/configurations/JwtSecurityConfig.java
+++ src/main/java/org/kylecodes/gm/configurations/JwtSecurityConfig.java
@@ -1,9 +1,8 @@
 package org.kylecodes.gm.configurations;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.kylecodes.gm.filters.JwtFilter;
 import org.kylecodes.gm.services.UserServiceImpl;
+import org.kylecodes.gm.utils.SpaCsrfTokenRequestHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.*;
-import org.springframework.util.StringUtils;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,7 +24,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 @Configuration
 @EnableWebSecurity
@@ -39,9 +36,8 @@ public class JwtSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
         CookieCsrfTokenRepository csrfTokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-        System.out.println("Domain name: " + System.getenv("DOMAIN_NAME"));
         csrfTokenRepository.setCookieCustomizer(csrfTokenRepo -> csrfTokenRepo
                 .sameSite("None")
                 .secure(true)
@@ -53,6 +49,7 @@ public class JwtSecurityConfig {
                 .csrfTokenRepository(csrfTokenRepository)
                 .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
         );
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         http.authorizeHttpRequests(
                 auth -> {
                     auth.requestMatchers("/register", "/auth/login", "/auth/logout", "/csrf-cookie").permitAll()
@@ -60,8 +57,6 @@ public class JwtSecurityConfig {
                     auth.anyRequest().authenticated();
                 });
         http.logout(AbstractHttpConfigurer::disable);
-//
-//        http.addFilterBefore(new CsrfLoggingFilter(), CsrfFilter.class);
 
         http.sessionManagement(
                 session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -69,42 +64,7 @@ public class JwtSecurityConfig {
 
         return http.build();
     }
-    final class SpaCsrfTokenRequestHandler implements CsrfTokenRequestHandler {
-        private final CsrfTokenRequestHandler plain = new CsrfTokenRequestAttributeHandler();
-        private final CsrfTokenRequestHandler xor = new XorCsrfTokenRequestAttributeHandler();
 
-        @Override
-        public void handle(HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
-            /*
-             * Always use XorCsrfTokenRequestAttributeHandler to provide BREACH protection of
-             * the CsrfToken when it is rendered in the response body.
-             */
-            this.xor.handle(request, response, csrfToken);
-            /*
-             * Render the token value to a cookie by causing the deferred token to be loaded.
-             */
-            csrfToken.get();
-            System.out.println(csrfToken.get());
-            response.addHeader("X-CSRF-Debug-Handled", "true");
-        }
-
-        @Override
-        public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
-            String headerValue = request.getHeader(csrfToken.getHeaderName());
-            /*
-             * If the request contains a request header, use CsrfTokenRequestAttributeHandler
-             * to resolve the CsrfToken. This applies when a single-page application includes
-             * the header value automatically, which was obtained via a cookie containing the
-             * raw CsrfToken.
-             *
-             * In all other cases (e.g. if the request contains a request parameter), use
-             * XorCsrfTokenRequestAttributeHandler to resolve the CsrfToken. This applies
-             * when a server-side rendered form includes the _csrf request parameter as a
-             * hidden input.
-             */
-            return (StringUtils.hasText(headerValue) ? this.plain : this.xor).resolveCsrfTokenValue(request, csrfToken);
-        }
-    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
